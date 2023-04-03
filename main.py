28d6aade156b1fbdce2e7b646c23a4911357026b
Kalween <kalin.joel@gmail.com>
2023-04-03 22:37:24 +0200

    No changes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
-GOOGLE_SHEET = 'https://docs.google.com/forms/d/e/1FAIpQLSfSrOvqYVvHOjEJ8YECmhc-3gQ-chLSRiYC2HRkXiAiiRe5ug/viewform?usp=sf_link'
+GOOGLE_SHEET = 'INSERT DOCS LINK'
 HEMNET = 'https://www.zillow.com/wenatchee-wa/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22north%22%3A47.5150650462814%2C%22east%22%3A-119.87778908831359%2C%22south%22%3A47.28523555465401%2C%22west%22%3A-120.43191201311828%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A27836%2C%22regionType%22%3A6%7D%5D%2C%22mapZoom%22%3A12%7D'
 LINK = 'https://www.zillow.com/'
 headers = {
